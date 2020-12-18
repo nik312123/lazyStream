fee14d0b101223d3fe03ea0110c4ee52f4ddff95
Nikunj Chawla <nikchawla312@gmail.com>
2020-12-18 13:14:07 -0600

    Added GNU LGPLv3 license

--- lazyStream.ml
+++ lazyStream.ml
@@ -1,3 +1,25 @@
+(*
+    [LazyStream] is a data structure that is like {{:https://tinyurl.com/ocaml-seq-mod} Seq} and
+    {{:https://tinyurl.com/ocaml-stream-mod} Stream} but more lazily evaluated, though calling a function with the same
+    parameters multiple times will create different streams and will be evaluated separately, and more provided
+    functions
+    
+    Copyright (C) 2020 Nikunj Chawla
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*)
+
 type 'a t = Nil | Cons of 'a * 'a t lazy_t
 
 let empty: 'a t = Nil
